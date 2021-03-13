0a7304fe73cba64718cf47e4ee5a2420c3099474
vald3nir <vald3nir@gmail.com>
2021-03-13 08:59:05 -0300

    extract movie IDs

---
+++ tools/ids_extractor/extractor.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import os
+
+movies_folder = "/media/vald3nir/Arquivos/Vald3flix/Filmes"
+
+ids = {""}
+ids.clear()
+
+for root, dirs, files in os.walk(movies_folder):
+    for s in files:
+        ids.add("tt" + s[s.find('[tt') + len('[tt'):s.rfind(']')])
+
+with open('ids.txt', 'w') as file:
+    file.write("[")
+    for key in ids:
+        file.write(f"'{key}',")
+    file.write("]")
+
+print(f"{len(ids)} Movies")
